f6c9574f0a52edf299da63b5c0dff6329c0be4b9
OlAndrey <oleynik.andrey01@gmail.com>
2023-03-30 23:14:43 +0300

    chore: MongoDB was configured

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const { mongoose } = require('mongoose')
 
-
+const PORT = process.env.PORT || 5500
 const server = express()
 
-
 // Server Setup
-const port = process.env.PORT || 8000
-server.listen(port, () => {
-    console.log(`Server listening on: ${port}`)
-})
+const start = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://OleynikAndrey01:9eglG96nfRima86G@cluster0.rgv4had.mongodb.net/?retryWrites=true&w=majority'
+    )
+
+    server.listen(PORT, () => {
+      console.log(`Server started on port: ${PORT}`)
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start()
