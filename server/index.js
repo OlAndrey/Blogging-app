ee0e1965ec446824ebf06b10de7c12514b71f35a
OlAndrey <oleynik.andrey01@gmail.com>
2023-04-25 22:15:40 +0300

    refactor(server): preparation for deploy to Vercel

--- server/index.js
+++ server/index.js
@@ -1,46 +1,40 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const http = require('http')
 const fileUpload = require('express-fileupload')
-const { mongoose } = require('mongoose')
-const dotenv = require('dotenv')
-const cors = require('cors')
+// const dotenv = require('dotenv')
+// const cors = require('cors')
 const authRoute = require('./routes/auth')
 const postRoute = require('./routes/post')
 const commentRoute = require('./routes/comment')
 
-const server = express()
-dotenv.config()
+const app = express()
+// dotenv.config()
 
-const PORT = process.env.PORT || 5500
-const user = process.env.DB_USER
-const password = process.env.DB_PASSWORD
+const port = process.env.PORT || 3000
+// const user = process.env.DB_USER
+// const password = process.env.DB_PASSWORD
 
-server.use(
-  cors({
-    origin: ['http://localhost:3000', 'http://localhost:1234']
-  })
-)
+// app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }))
+app.use(fileUpload())
+app.use(express.json())
+app.use(express.static('uploads'))
+app.use('/api/auth', authRoute)
+app.use('/api/post', postRoute)
+app.use('/api/comment', commentRoute)
 
-// server.use(bodyParser.urlencoded({ limit: '5mb', extended: true }))
-server.use(fileUpload())
-server.use(express.json())
-server.use(express.static('uploads'))
-server.use('/api/auth', authRoute)
-server.use('/api/post', postRoute)
-server.use('/api/comment', commentRoute)
+const server = http.createServer(app)
 
 // Server Setup
-const start = async () => {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://${user}:${password}@cluster0.rgv4had.mongodb.net/?retryWrites=true&w=majority`
-    )
-
-    server.listen(PORT, () => {
-      console.log(`Server started on port: ${PORT}`)
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('MongoDB connected!')
+    server.listen(port, () => {
+      console.log(`Server started on port: ${port}`)
     })
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-start()
+  })
+  .catch((err) => {
+    console.log({ err })
+    process.exit(1)
+  })
